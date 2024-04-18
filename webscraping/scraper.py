3cb17a8389df5784fb41cc24518a0e3ac0f49654
Tsuquon <liamling04@gmail.com>
2024-04-18 17:59:27 +1000

    club parsing

--- webscraping/scraper.py
+++ webscraping/scraper.py
@@ -12,9 +12,30 @@ def get_clubs(clubs):
     except(ValueError):
         print("Non-club websites not removed")
         
+    club_processing(clubs)
+        
+        
+# needs club name, description, membership cost
+def club_processing(clubs: list):
+    url = "https://usu.edu.au/clubs/wasabi-japanese-cultural/"
+    pattern = r">.*</"
+    
+    page_content = requests.get(url).content
+    soup = BeautifulSoup(page_content, "html.parser")
+    title = soup.find("h1", {"class": "Club-module--title--4dc7e"})
+    title = re.search(pattern, str(title)).group()[1:-2]
+    membership_fee = soup.find("div", {"class": "Club-module--fee--18235"})
+    membership_fee = int(re.search(r"\$\d", str(membership_fee)).group()[1:])
+    
+    description = soup.find("div", {"class": "Club-module--mainContainer--4e43a"})
+    description = description.find_all("p")
+    description = re.sub(r"</*p>,*", "\\n", str(description))[1:-1]
+
+    print(title, description, membership_fee)
+        
 def get_events(events):
     events = [text for text in events if re.match("^https://usu.edu.au/events", text)]
-    print(events)
+    # print(events)
     
     try:
         events.remove("https://usu.edu.au/events/club-events/")
@@ -24,8 +45,7 @@ def get_events(events):
     except(ValueError):
         print("Non-club websites not removed")
 
-def source_information(scraped_links):
-    pass
+
 
 
 def main():
@@ -38,8 +58,8 @@ def main():
     pattern = r"(<loc>)|(</loc>)"
     web_links = [re.sub(pattern, "", str(text)) for text in web_links]
     
-    # get_clubs(web_links)
-    get_events(web_links)
+    get_clubs(web_links)
+    # get_events(web_links)
     
 
 if __name__ == "__main__":
