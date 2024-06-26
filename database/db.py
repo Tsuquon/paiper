bb0c26e59b44c3ed643d82aed0284fd10f414120
Bernard Tam <bernard@bernent.co.uk>
2024-06-26 10:21:34 +1000

    Created function to register a user

--- database/db.py
+++ database/db.py
@@ -28,7 +28,60 @@ def retrieveUsers(connection_string):
 
     connection.close()
 
-retrieveUsers(connection_string)
+def createNewUser(connection_string, user_data, user_preferences):
+    row = {
+        "email": user_data["email"],
+        "password": user_data["password"],
+        "first_name": user_data["first_name"],
+        "last_name": user_data["last_name"],
+        "misc_information": user_data["misc_information"]
+    }
+
+    engine = create_engine(connection_string)
+    connection = engine.connect()
+
+    sql_query = text("INSERT INTO users (email, password, first_name, last_name, misc_information) VALUES (:email, :password, :first_name, :last_name, :misc_information)")
+    connection.execute(sql_query, row)
+
+    sql_query = text("SELECT user_id FROM users WHERE email = :email")
+    result = connection.execute(sql_query, row)
+
+    user_id_row = result.fetchone()
+    if not user_id_row:
+        print("Error: user not created.")
+        connection.close()
+        return
+        
+    created_user_id = user_id_row[0]
+        
+    tag_ids = []
+    for user_preference in user_preferences:
+        sql_query = text("SELECT tag_id FROM tags WHERE tag = :tag")
+        result = connection.execute(sql_query, {"tag": user_preference})
+
+        tag_id_row = result.fetchone()
+        if tag_id_row:
+            tag_ids.append(tag_id_row[0])
+
+    for tag_id in tag_ids:
+        sql_query = text("INSERT INTO userPreferences (user_id, tag_id) VALUES (:user_id, :tag_id)")
+        connection.execute(sql_query, {"user_id": created_user_id, "tag_id": tag_id})
+
+    connection.commit()
+    connection.close()
+
+user_data = {
+    'email': 'johndoe@example.com',
+    'password': 'password',
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'misc_information': ''
+}
+
+user_preferences = ["LAW", "ECONOMICS"]
+
+createNewUser(connection_string, user_data, user_preferences)
+
 
 """
 call the webscraping function from Liam which returns events in a tuple -> (event_name, event_description, event_start_date, event_end_date, event_location, event_price)
